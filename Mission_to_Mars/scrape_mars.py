d6acbeefe5796739866715e60189cce405237bb2
nbolt1989 <ncbolt89@gmail.com>
2020-08-26 12:38:52 -0400

    pushing final changes

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -7,11 +7,16 @@ from splinter import Browser
 import time
 import pandas as pd
 
+#Set up my main function, this will return my main dictionary
 def scrape():
-    
+    # exectuable path and browser
     executable_path = {"executable_path": "chromedriver.exe"}
     browser = Browser('chrome', executable_path, headless=False)
+
+    # set my news title and paragraph variable
     news_title, news_para  = scrape_mars_news(browser)
+
+    # return my dictionary which will encompass my title, para, featured image, mars facts, and hemispheres
     return{
         "News_Title": news_title,
         "Paragraph": news_para,
@@ -20,6 +25,7 @@ def scrape():
         "Hemispheres": mars_hemis(browser)
     }
 
+# my function for scraping mars news title and paragraph; this will be almost identical to my Jupyter notebook
 def scrape_mars_news(browser):
     url = "https://mars.nasa.gov/news/"
     browser.visit(url)
@@ -36,6 +42,7 @@ def scrape_mars_news(browser):
 
     return news_title, news_para
 
+# my function for grabbing the featured image; this will be almost identical to my Jupyter notebook
 def featured_image_url(browser):
     imageurl = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(imageurl)
@@ -50,11 +57,10 @@ def featured_image_url(browser):
 
     return featured_image_url
 
+# my function for scraping mars facts; this will be almost identical to my Jupyter notebook
 def mars_facts(browser):
     
-#create a new URL for JPL
-    
-    
+    #create a new URL for JPL
     JPLurl = "https://space-facts.com/mars/"
     # browse the article
     
@@ -65,6 +71,7 @@ def mars_facts(browser):
     
     return final_table.to_html()
 
+# my function for scraping for the mars hemispheres; this will be almost identical to my Jupyter notebook
 def mars_hemis(browser):
     
     astro_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
@@ -103,16 +110,3 @@ def mars_hemis(browser):
         browser.back()
     
     return hemi_list
-    
-        
-
-    #hemi_df = pd.DataFrame(hemi_list)
-    #for x in hemi_df['imageURL']:
-        #urls = [x]
-        
-        #return urls
-    #for x in hemi_list:
-        #url = x
-        #return url'''
-
-
